9c680b2cb44cee6e561a969de0777dab691a9f44
VagrantPi <xup6u06m4@gmail.com>
2025-09-16 05:50:46 +0800

    fix: 修正部署遇到的問題

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    // Avoid requiring full TypeScript program for lint to reduce tooling friction
+    tsconfigRootDir: __dirname,
     sourceType: "module",
   },
   ignorePatterns: [
@@ -26,8 +27,23 @@ module.exports = {
     "import",
   ],
   rules: {
-    "quotes": ["error", "double"],
+    "quotes": ["error", "double", {"avoidEscape": true, "allowTemplateLiterals": true}],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "require-jsdoc": 0,
+    "max-len": ["error", {"code": 120}],
+    "object-curly-spacing": 0,
+    "space-before-function-paren": 0,
   },
+  overrides: [
+    {
+      files: ["test/**/*.ts"],
+      rules: {
+        "require-jsdoc": 0,
+        "quotes": 0,
+        "object-curly-spacing": 0,
+        "max-len": 0,
+      },
+    },
+  ],
 };
